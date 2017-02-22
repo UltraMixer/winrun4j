81fa1cbedd593fb6f8d7903f0f67e2ea43fddaa9
Matthias Hänel <haenel@ultramixer.com>
2017-02-22 21:30:49 +0100

    ATI high performance support
    enables the high performance support for ATI GPUs

--- WinRun4J/src/WinRun4J.cpp
+++ WinRun4J/src/WinRun4J.cpp
@@ -34,6 +34,12 @@ extern "C" {
 	_declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
 }
 
+//ATI please also give us the high performance GPU when possible
+extern "C"
+{
+	__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
+
 namespace 
 {
 	TCHAR *vmargs[MAX_PATH];
